c93e60afdcfd0c87da44b405b3e5553cd3946caa
KaamouchZoubair <zoubair.res@gmail.com>
2020-03-02 00:26:10 +0100

    commit asp page

---
+++ ASP_Projet/ASP_Projet/Controllers/ComputerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP_Projet.Models; 
+
+namespace ASP_Projet.Controllers
+{
+    public class ComputerController : Controller
+    {
+        // GET: Computer
+        public ActionResult Afficher()
+        {
+            var mycomputer = new Computer
+            {
+                Name = "Lonovo",
+                Price = 1500
+            };
+            return View(mycomputer);
+        }
+    }
+}
---
+++ ASP_Projet/ASP_Projet/Models/Computer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_Projet.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
