06ac29acca099da077f0fc50910b797d709b2f30
moad khalal <m5.tech@outlook.com>
2020-04-28 02:48:53 +0200

    ajout de input number

--- exemple_mvc/exemple_mvc/App_Start/RouteConfig.cs
+++ exemple_mvc/exemple_mvc/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace exemple_mvc
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Accueil", id = UrlParameter.Optional }
+                defaults: new { controller = "services", action = "Accueil", id = UrlParameter.Optional }
             );
         }
     }
